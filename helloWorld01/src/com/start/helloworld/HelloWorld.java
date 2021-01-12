58d8bd1e11707f07ec55300f9c463e58186c84ee
huang <qijun0905@126>
2021-01-12 20:26:46 +0800

    first demo start hello world

--- helloWorld01/src/com/start/helloworld/HelloWorld.java
+++ helloWorld01/src/com/start/helloworld/HelloWorld.java
@@ -1,6 +1,22 @@
 package com.start.helloworld;
 
+// 这是一行注释信息
+// 单行注释
+
+/*
+多行注释
+ */
+
+/**
+ * 第一行的第三个单词必须和所在的文件名完全一致，大小写也要一样
+ * public class后面代表定义一个类的名称，类是java中所有源代码的基本组织单位。
+ *
+ * 第二行的内容是固定写法，代表main方法，是程序的执行起点
+ *
+ * 第三行打印输出语句，固定不变
+ */
 public class HelloWorld {
+
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
